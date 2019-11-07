59bf6d29c114f656385d009a5fcda1dc75498584
haminthecoder <jham.lee@mail.utoronto.ca>
2019-11-07 00:06:43 -0500

    11.7 Udemy 84-86

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manage-api',  {
     useNewUrlParser: true,
@@ -7,30 +8,78 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manage-api',  {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+
+
+        }
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be a positive number')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 7,
+        validate(value) {
+            if (validator.contains(value.toLowerCase(), "password")) {
+                throw new Error("The Password should not contain password")
+            }
+        }
     }
 });
+
+// const me = new User({
+//     name: "   John   ",
+//     email: "fohn@bluroot.com   ",
+//     password: "abfffdd"
+// })
+//
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((err) => {
+//     console.log(err)
+// })
+
 
 
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
 })
 
-const task = new Task({
-    description: "Learn how to use the Mongoose Library",
-    completed: false
-});
-
-task.save().then(() => {
-    console.log(task)
-}).catch((error) => {
-    console.log(error)
-})
+// const task = new Task({
+//     description: "Learn how to use the Mongoose Library",
+//     completed: false
+// });
+//
+// task.save().then(() => {
+//     console.log(task)
+// }).catch((error) => {
+//     console.log(error)
+// })
