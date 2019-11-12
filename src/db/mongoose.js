d42a22c573db34ee93fc5457af62ce86286f158d
haminthecoder <jham.lee@mail.utoronto.ca>
2019-11-12 14:24:30 -0500

    Resturcure a Users & Tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,51 +1,13 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manage-api',  {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-const User = mongoose.model('User', {
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
-            }
 
 
-        }
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if (validator.contains(value.toLowerCase(), "password")) {
-                throw new Error("The Password should not contain password")
-            }
-        }
-    }
-});
 
 // const me = new User({
 //     name: "   John   ",
@@ -59,19 +21,6 @@ const User = mongoose.model('User', {
 //     console.log(err)
 // })
 
-
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-})
 
 // const task = new Task({
 //     description: "Learn how to use the Mongoose Library",
