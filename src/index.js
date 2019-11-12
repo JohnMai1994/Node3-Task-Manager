9893a9c444deda68cd0985b92cf7b384c1c1a1be
haminthecoder <jham.lee@mail.utoronto.ca>
2019-11-12 15:02:10 -0500

    Resturcure a Users & Tasks

--- src/index.js
+++ src/index.js
@@ -21,9 +21,22 @@ app.get("/users", (req, res) => {
     User.find({}).then((users) => {
         res.status(200).send(users)
     }).catch((e) => {
-
+        res.status(500).send()
     })
-})
+});
+
+app.get("/users/:id", (req, res)=> {
+    const _id = req.params.id;
+    User.findById({_id}).then((user) => {
+        if (!user) {
+            return res.status(404).send("This Id Not Exist ");
+        }
+        res.status(200).send(user)
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+});
+
 
 app.post('/task', (req, res) => {
     const task = new Task(req.body);
@@ -32,7 +45,30 @@ app.post('/task', (req, res) => {
     }).catch( (err) => {
         res.status(400).send(err);
     })
+});
+
+app.get('/task', (req, res) => {
+    Task.find({}).then((task) => {
+        res.status(200).send(task);
+    }).catch((err) => {
+        res.status(500).send(err);
+    })
+});
+
+app.get('/task/:id', (req, res) => {
+    const _id = req.params.id;
+    Task.findById({_id}).then((task)=> {
+        if (!task) {
+            return res.status(400).send("The id not exist");
+        }
+
+        res.status(200).send(task);
+    }).catch((err)=> {
+        res.status(500).send(err);
+    })
 })
+
+
 
 app.listen(port, () => {
     console.log("Server is up on port "+ port);
